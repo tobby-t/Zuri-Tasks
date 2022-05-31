531e7c0b6774e63ba1cf41e2c8e9a18a0a4f73c4
tobby-t <105219273+tobby-t@users.noreply.github.com>
2022-05-31 14:26:32 -0700

    done

---
+++ Tobiloba_Tolani_Lawal/Tobiloba_Tolani_Lawal/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Tobiloba_Tolani_Lawal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tobiloba_Tolani_Lawal.settings')
+
+application = get_wsgi_application()
